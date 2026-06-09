09f43923393986576275caf5b0404070ea19075c
vamsi <vijayvamsi550@gmail.com>
2026-06-09 10:21:22 +0530

    hello

---
+++ 01-mongodb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+source ./common.sh
+
+check_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+#        ^         ^
+#        |         |
+#        $1        $2
+
+dnf install mongodb-org -y &>> $LOGS_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable --now mongod
+VALIDATE $? "Starting and enabling MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Resarting MongoDB"
+
+print_total_time
+
