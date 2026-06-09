8ba15cda78d1e68b8be7421b29126ec3928efe33
vamsi <vijayvamsi550@gmail.com>
2026-06-09 15:19:41 +0530

    hello

---
+++ 03-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./common
+
+check_root
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>> $LOGS_FILE
+systemctl start mysqld &>> $LOGS_FILE  
+VALIDATE $? "Enable and start MySQL Server"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password"
+
+print_total_time
