b006eaa4d3a935cecb28ad42637297483696f4e9
vamsi <vijayvamsi550@gmail.com>
2026-06-09 15:30:09 +0530

    hello

---
+++ 05.catalogue.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+source ./common.sh
+
+check_root
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+dnf install nodejs -y  &>> $LOGS_FILE
+VALIDATE $? "Installing NodeJS:20"
+
+id roboshop &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y Skipping $N "
+fi
+
+rm -rf /app
+VALIDATE $? "Removing existing code"
+
+rm -rf /tmp/catalogue.zip 
+VALIDATE $? "Removing catalogue zip"
+
+mkdir -p /app &>> $LOGS_FILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGS_FILE
+cd /app 
+unzip /tmp/catalogue.zip &>> $LOGS_FILE
+VALIDATE $? "Downloaded and extracted catalogue code"
+
+npm install &>> $LOGS_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Created systemctl service"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Added Mongo repo"
+
+dnf install mongodb-mongosh -y &>> $LOGS_FILE
+VALIDATE $? "Installed MongoDB client"
+
+INDEX=$(mongosh --host mongodb.devops550.online --quiet \ --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -lt 0 ]; then 
+    mongosh --host mongodb.devops550.online </app/db/master-data.js &>> $LOGS_FILE
+    VALIDATE $? "Load Products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi 
+
+systemctl enable catalogue &>> $LOGS_FILE
+systemctl restart catalogue &>> $LOGS_FILE
+VALIDATE $? "Restarting catalogue" 
+
+print_total_time
